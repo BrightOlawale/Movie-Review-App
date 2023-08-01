3938056912044baf45eb46318be121c0b9f95dda
BrightOlawale <oolawalebright@gmail.com>
2023-08-01 17:33:54 +0100

    refactor: Error handling

--- backend/api/movies.controller.js
+++ backend/api/movies.controller.js
@@ -4,42 +4,47 @@ import MovieDAO from '../dao/moviesDAO.js'
 export default class MovieController {
   // Create static method to handles the API request to get a list of movies
   static async apiGetMovies (req, res) {
-    // Check if moviesPerPage and page query parameters are present in the request query string
-    // If they are not present, define thier values
-    const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage, 10) : 20
-    const page = req.query.page ? parseInt(req.query.page, 10) : 0
+    try {
+      // Check if moviesPerPage and page query parameters are present in the request query string
+      // If they are not present, define thier values
+      const moviesPerPage = req.query.moviesPerPage ? parseInt(req.query.moviesPerPage, 10) : 20
+      const page = req.query.page ? parseInt(req.query.page, 10) : 0
 
-    // Create a filters object to store the db query parameters
-    const filters = {}
+      // Create a filters object to store the db query parameters
+      const filters = {}
 
-    // Check if rated query parameter is present in the request query string
-    // If it is present, add it to the filters object
-    // Else chrck if title query parameter is present in the request query string
-    // If it is present, add it to the filters object
-    if (req.query.rated) {
-      filters.rated = req.query.rated
-    } else if (req.query.title) {
-      filters.title = req.query.title
+      // Check if rated query parameter is present in the request query string
+      // If it is present, add it to the filters object
+      // Else chrck if title query parameter is present in the request query string
+      // If it is present, add it to the filters object
+      if (req.query.rated) {
+        filters.rated = req.query.rated
+      } else if (req.query.title) {
+        filters.title = req.query.title
+      }
+
+      // Now pass the filters object to the getMovies static method of the MovieDAO class and store the returned object in moviesList
+      // and totalNumMovies object variables
+      const { moviesList, totalNumMovies } = await MovieDAO.getMovies({
+        page,
+        moviesPerPage,
+        filters
+      })
+
+      // Now create a response object to send back to the client if query was successful
+      const response = {
+        movies: moviesList,
+        page,
+        filters,
+        entries_per_page: moviesPerPage,
+        total_results: totalNumMovies
+      }
+
+      // Send the response object back to the client ans send status code
+      res.status(200).json(response)
+    } catch (error) {
+      console.error(`Error while processing the request: ${error}`)
+      res.status(500).json({ error: error.message })
     }
-
-    // Now pass the filters object to the getMovies static method of the MovieDAO class and store the returned object in moviesList
-    // and totalNumMovies object variables
-    const { moviesList, totalNumMovies } = await MovieDAO.getMovies({
-      page,
-      moviesPerPage,
-      filters
-    })
-
-    // Now create a response object to send back to the client
-    const response = {
-      movies: moviesList,
-      page,
-      filters,
-      entries_per_page: moviesPerPage,
-      total_results: totalNumMovies
-    }
-
-    // Send the response object back to the client
-    res.json(response)
   }
 }
--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -4,9 +4,7 @@ import MovieController from './movies.controller.js'
 // Get access to express router
 const router = express.Router()
 
-//
-router.route('/').get((req, res) => {
-  res.send(MovieController.apiGetMovies)
-})
+// Index page
+router.route('/').get(MovieController.apiGetMovies)
 
 export default router
