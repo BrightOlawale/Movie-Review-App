2ad6efd46ef0ca124d0e3a226601407b620ba5c2
BrightOlawale <oolawalebright@gmail.com>
2023-08-02 08:31:24 +0100

    feat: Review route

--- backend/api/movies.route.js
+++ backend/api/movies.route.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import MovieController from './movies.controller.js'
+import ReviewController from './reviews.controller.js'
 
 // Get access to express router
 const router = express.Router()
 
-// Index page
+// Route to get a list of movies
 router.route('/').get(MovieController.apiGetMovies)
+
+// Route to post review, update review and delete review
+router.route("/review")
+            .post(ReviewController.apiPostReview)
+            .put(ReviewController.apiUpdateReview)
+            .delete(ReviewController.apiDeleteReview)
 
 export default router
