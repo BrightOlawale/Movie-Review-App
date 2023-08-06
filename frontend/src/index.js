ae5e2d6b8c8b36aa93c4a8f47f6dfa18ebefed9e
BrightOlawale <oolawalebright@gmail.com>
2023-08-06 22:37:49 +0100

    feat: Frontend setup

---
+++ frontend/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// serviceWorker.unregister() is a function that is called to unregister a service worker.
+// A service worker is a script that your browser runs in the background, separate from a web page,
+// The serviceWorker.register() method is meant to create progressive web apps (PWA) 
+// catered more to mobile React native applications for offline work.
+// In this case, we are not using a service worker, so we unregister it.
+serviceWorker.unregister();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
