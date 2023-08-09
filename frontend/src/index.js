0d79508fac7a12b5957ee41b4e8281d403441edc
BrightOlawale <oolawalebright@gmail.com>
2023-08-09 15:45:21 +0100

    feat: BrowserRouter setup

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,19 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import BrowserRouter from react-router-dom
+// The BrowserRouter navigates the routes we have created in the App.js file.
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
 
-// serviceWorker.unregister() is a function that is called to unregister a service worker.
-// A service worker is a script that your browser runs in the background, separate from a web page,
-// The serviceWorker.register() method is meant to create progressive web apps (PWA) 
-// catered more to mobile React native applications for offline work.
-// In this case, we are not using a service worker, so we unregister it.
-serviceWorker.unregister();
+// // serviceWorker.unregister() is a function that is called to unregister a service worker.
+// // A service worker is a script that your browser runs in the background, separate from a web page,
+// // The serviceWorker.register() method is meant to create progressive web apps (PWA) 
+// // catered more to mobile React native applications for offline work.
+// // In this case, we are not using a service worker, so we unregister it.
+// serviceWorker.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
